bc7c8dfb5435431e9ec5aa9cf5b52b72a3542c60
Vivek <vivektheintel@gmail.com>
2021-07-30 09:07:07 +0530

    Including bat file for windows launcher

---
+++ bin/win/twm-mkt-eq-service.bat
@@ -0,0 +1,11 @@
+set APP_NAME="twm-mkt-eq-service"
+set APP_VERSION="1.0.0-SNAPSHOT"
+set JAVA_PARAM="-Xmx101m"
+
+set BIN_PATH="%PROM_HOME_PARENT%/TWM/%APP_NAME%/bin"
+set JAR_PATH="%BIN_PATH%/../target/quarkus-app/quarkus-run.jar"
+
+set APP_PARAMS="-Dequity.holdingsFileLocation=%BIN_PATH%/../landing/holdings.csv -Dequity.executionIntervalDuration=$1"
+
+echo "Starting %APP_NAME% with java param: %JAVA_PARAM%, app params: %APP_PARAMS% at %JAR_PATH%"
+java %JAVA_PARAM% %APP_PARAMS% -jar %JAR_PATH%
